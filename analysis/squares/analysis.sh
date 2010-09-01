98f48528ad2fa28112a3f9aa0c6ce43b920fa1fa
Stephen Bell <stephen.david.bell@googlemail.com>
2010-09-02 00:21:14 +0100

    Changed awk line formatting to mean/min/max

--- analysis/squares/analysis.sh
+++ analysis/squares/analysis.sh
@@ -8,25 +8,25 @@ echo Sitting:
 grep sitting -A4 results-aggregated.txt | \
 awk '/30:/' | \
 cut -d' ' -f2 | \
-awk -F'/' '{a+=$1; b+=$2; c+=$3; d+=$4; ++n} END {printf "30,%.3f,%.3f,%.3f,%.3f\n", (b/n), (a/n), c/n, d/n}' | \
+awk -F'/' '{a+=$1; b+=$2; c+=$3; d+=$4; ++n} END {printf "30,%.3f,%.3f,%.3f,%.3f\n", (a/n), (b/n), c/n, d/n}' | \
 tee -a sitting.dat
 
 grep sitting -A4 results-aggregated.txt | \
 awk '/60:/' | \
 cut -d' ' -f2 | \
-awk -F'/' '{a+=$1; b+=$2; c+=$3; d+=$4; ++n} END {printf "60,%.3f,%.3f,%.3f,%.3f\n", (b/n), (a/n), c/n, d/n}' | \
+awk -F'/' '{a+=$1; b+=$2; c+=$3; d+=$4; ++n} END {printf "60,%.3f,%.3f,%.3f,%.3f\n", (a/n), (b/n), c/n, d/n}' | \
 tee -a sitting.dat
 
 grep sitting -A4 results-aggregated.txt | \
 awk '/99:/' | \
 cut -d' ' -f2 | \
-awk -F'/' '{a+=$1; b+=$2; c+=$3; d+=$4; ++n} END {printf "99,%.3f,%.3f,%.3f,%.3f\n", (b/n), (a/n), c/n, d/n}' | \
+awk -F'/' '{a+=$1; b+=$2; c+=$3; d+=$4; ++n} END {printf "99,%.3f,%.3f,%.3f,%.3f\n", (a/n), (b/n), c/n, d/n}' | \
 tee -a sitting.dat
 
 grep sitting -A4 results-aggregated.txt | \
 awk '/129:/' | \
 cut -d' ' -f2 | \
-awk -F'/' '{a+=$1; b+=$2; c+=$3; d+=$4; ++n} END {printf "129,%.3f,%.3f,%.3f,%.3f\n", (b/n), (a/n), c/n, d/n}' | \
+awk -F'/' '{a+=$1; b+=$2; c+=$3; d+=$4; ++n} END {printf "129,%.3f,%.3f,%.3f,%.3f\n", (a/n), (b/n), c/n, d/n}' | \
 tee -a sitting.dat
 
 echo Walking:
@@ -34,25 +34,25 @@ echo Walking:
 grep walking -A4 results-aggregated.txt | \
 awk '/30:/' | \
 cut -d' ' -f2 | \
-awk -F'/' '{a+=$1; b+=$2; c+=$3; d+=$4; ++n} END {printf "30,%.3f,%.3f,%.3f,%.3f\n", (b/n), (a/n), c/n, d/n}' | \
+awk -F'/' '{a+=$1; b+=$2; c+=$3; d+=$4; ++n} END {printf "30,%.3f,%.3f,%.3f,%.3f\n", (a/n), (b/n), c/n, d/n}' | \
 tee -a walking.dat
 
 grep walking -A4 results-aggregated.txt | \
 awk '/60:/' | \
 cut -d' ' -f2 | \
-awk -F'/' '{a+=$1; b+=$2; c+=$3; d+=$4; ++n} END {printf "60,%.3f,%.3f,%.3f,%.3f\n", (b/n), (a/n), c/n, d/n}' | \
+awk -F'/' '{a+=$1; b+=$2; c+=$3; d+=$4; ++n} END {printf "60,%.3f,%.3f,%.3f,%.3f\n", (a/n), (b/n), c/n, d/n}' | \
 tee -a walking.dat
 
 grep walking -A4 results-aggregated.txt | \
 awk '/99:/' | \
 cut -d' ' -f2 | \
-awk -F'/' '{a+=$1; b+=$2; c+=$3; d+=$4; ++n} END {printf "99,%.3f,%.3f,%.3f,%.3f\n", (b/n), (a/n), c/n, d/n}' | \
+awk -F'/' '{a+=$1; b+=$2; c+=$3; d+=$4; ++n} END {printf "99,%.3f,%.3f,%.3f,%.3f\n", (a/n), (b/n), c/n, d/n}' | \
 tee -a walking.dat
 
 grep walking -A4 results-aggregated.txt | \
 awk '/129:/' | \
 cut -d' ' -f2 | \
-awk -F'/' '{a+=$1; b+=$2; c+=$3; d+=$4; ++n} END {printf "129,%.3f,%.3f,%.3f,%.3f\n", (b/n), (a/n), c/n, d/n}' | \
+awk -F'/' '{a+=$1; b+=$2; c+=$3; d+=$4; ++n} END {printf "129,%.3f,%.3f,%.3f,%.3f\n", (a/n), (b/n), c/n, d/n}' | \
 tee -a walking.dat
 
 gnuplot 'plotter.p'
