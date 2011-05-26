76c6f8bbe6f3cc345f5c298e6042a47790a86b69
Stephen Bell <stephen.david.bell@googlemail.com>
2011-05-26 19:13:27 +0100

    Service now starts when SD card finishes mounting

---
+++ ContextCapture/src/com/nodobo/capture/MediaMountedReceiver.java
@@ -0,0 +1,33 @@
+package com.nodobo.capture;
+
+import android.os.Bundle;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ComponentName;
+import android.util.Log;
+
+public class MediaMountedReceiver extends BroadcastReceiver
+{
+    private static final String TAG = "MediaMountedReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        String action = intent.getAction();
+        Logger.log("ContextCapture", action);
+        if(action == null)
+            Log.e(TAG,"Action==null!");
+        else if("android.intent.action.MEDIA_MOUNTED".equals(action))
+        {
+            Intent i = new Intent();
+            i.setClassName("com.nodobo.capture", "com.nodobo.capture.ContextCapture");
+            
+            ComponentName cname = context.startService(i);
+            if(cname == null)
+                Log.e(TAG,"BackgroundService was not started");
+            else
+                Logger.log("ContextCapture","BackgroundService started");
+        }
+    }
+}
