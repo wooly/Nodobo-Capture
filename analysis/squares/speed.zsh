078ec6da694c0138c56a8d6dc34bfb67cd4eae8b
Stephen Bell <stephen.bell@strath.ac.uk>
2011-05-27 11:26:25 +0100

    Forgot to commit these too

--- analysis/squares/speed.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-(for x in results/sitting?.csv results/walking?.csv; do echo $x; python speed.py $x clues.sqlite3; done ) > speed.dat
-(for x in results/*-*.csv; do echo $x; python speed.py $x second-clues.sqlite3; done ) >> speed.dat
-sh analysis.sh speed.dat
-gnuplot plotter-speed.p
--- analysis/squares/taps.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-(for x in results/sitting?.csv results/walking?.csv; do echo $x; python taps.py $x clues.sqlite3; done ) > taps.dat
-(for x in results/*-*.csv; do echo $x; python taps.py $x second-clues.sqlite3; done ) >> taps.dat
-sh analysis.sh taps.dat
-gnuplot plotter-taps.p
